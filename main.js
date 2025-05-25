b9906e77c18f1bbd4b4b286ce1696f5447bad869
Ibrohim Mubarok <befousmain@gmail.com>
2025-05-26 01:09:03 +0700

    asd

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import { getCookie, setCookieWithExpireHour } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.6/cookie.js";
+import { getCookie } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.6/cookie.js";
 import { postJSON } from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.6/api.js";
 
-const postBiasa = async (target_url, datajson, responseFunction) => {
+const postBiasa = async (target_url, responseFunction) => {
     try {
         const myHeaders = new Headers({
             "Content-Type": "application/json"
@@ -26,46 +26,14 @@ const postBiasa = async (target_url, datajson, responseFunction) => {
 
 const getSystemInfo = async () => {
     document.addEventListener("DOMContentLoaded", async () => {
-        if (getCookie("Tracker")) {
-            console.log("Data sudah dikirim dalam 24 jam terakhir, tidak mengirim ulang.");
-            return;
-        }
-        try {
-            const response = await fetch("https://api.ipify.org?format=json");
-            const { ip } = await response.json();
-            let hostnameWeb = window.location.hostname;
-            const urlHref = window.location.href;
-            let datajson
-
-            if (hostnameWeb === "t.if.co.id") {
-                const pathname = window.location.pathname;
-                hostnameWeb = `${hostnameWeb}${pathname}`;
-                datajson = {
-                    ipv4: ip,
-                    hostname: hostnameWeb,
-                    url: urlHref,
-                    browser: navigator.userAgent
-                };
-                await postBiasa("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/tracker/token", datajson, responseFunction);
-            } else {
-                datajson = {
-                    ipv4: ip,
-                    hostname: hostnameWeb,
-                    url: urlHref,
-                    browser: navigator.userAgent
-                };
-                await postBiasa("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/tracker/token", datajson, responseFunction);
-            }
-        } catch (error) {
-            console.error("Error mengambil IP dari ipify:", error);
-        }
+        await postBiasa("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/tracker/testing", responseFunction);
     });
 };
 
 function responseFunction(result) {
     if (result.status == 200) {
-        setCookieWithExpireHour("Tracker", result.data.response, 24);
-
+        console.log(result.data.response)
+        console.log(result.data.data)
         const trackerToken = getCookie("Tracker");
         if (trackerToken) {
             postJSON("https://asia-southeast2-awangga.cloudfunctions.net/domyid/api/tracker/testing", result.originalData, responseFunction2, "Tracker", trackerToken
@@ -74,12 +42,5 @@ function responseFunction(result) {
     }
 }
 
-function responseFunction2(result) {
-    if (result.status == 200) {
-        console.log("Berhasil")
-    } else {
-        console.log("Error")
-    }
-}
 
 getSystemInfo();
